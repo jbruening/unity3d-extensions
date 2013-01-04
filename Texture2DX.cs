412cb81716dd13fcb607fb5d3ff03dcc2b33f855
jbruening <robojoos@gmail.com>
2013-01-04 14:17:20 -0700

    fixes and changes to Texture2D extensions
    - fixed non-square textures throwing errors when clearing borders
    +added optional argument to make the texture no longer readable after
    clearing borders.

--- Texture2DX.cs
+++ Texture2DX.cs
@@ -9,7 +9,8 @@ namespace UEx
         /// </summary>
         /// <param name="texture"></param>
         /// <param name="clearColor"> </param>
-        public static void ClearMipMapBorders(this Texture2D texture, Color clearColor)
+        /// <param name="makeNoLongerReadable"> </param>
+        public static void ClearMipMapBorders(this Texture2D texture, Color clearColor, bool makeNoLongerReadable = false)
         {
             var mipCount = texture.mipmapCount;
 
@@ -18,34 +19,45 @@ namespace UEx
 
 
             var width = texture.width;
+            var height = texture.height;
             // tint each mip level
             for (var mip = 1; mip < mipCount; ++mip)
             {
                 var mipWidth = Mathf.Max(1, width >> mip);
+                var mipHeight = Mathf.Max(1, height >> mip);
                 if (mipWidth <= 2) continue; //don't change mip levels below 2x2
-                var cols = new Color[mipWidth]; //instantiates a new color array. default color is completely clear.
-                for (var i = 0; i < cols.Length; ++i)
+                var xCols = new Color[mipWidth];
+                var yCols = new Color[mipHeight];
+                if (clearColor != default(Color)) //speedup.
                 {
-                    cols[i] = clearColor;
+                    for (var x = 0; x < xCols.Length; ++x)
+                    {
+                        xCols[x] = clearColor;
+                    }
+                    for (var y = 0; y < yCols.Length; ++y)
+                    {
+                        yCols[y] = clearColor;
+                    }
                 }
-                texture.SetPixels(0, 0, mipWidth, 1, cols, mip); //set the top edge colors
-                texture.SetPixels(0, 0, 1, mipWidth, cols, mip); //set the left edge colors
-                texture.SetPixels(mipWidth - 1, 0, 1, mipWidth, cols, mip); //set the bottom edge colors
-                texture.SetPixels(0, mipWidth - 1, mipWidth, 1, cols, mip); //set the right edge colors
+                texture.SetPixels(0, 0, mipWidth, 1, xCols, mip); //set the top edge colors
+                texture.SetPixels(0, 0, 1, mipHeight, yCols, mip); //set the left edge colors
+                texture.SetPixels(mipWidth - 1, 0, 1, mipWidth, xCols, mip); //set the bottom edge colors
+                texture.SetPixels(0, mipWidth - 1, mipHeight, 1, yCols, mip); //set the right edge colors
             }
 
             // actually apply all SetPixels, don't recalculate mip levels
-            texture.Apply(false);
+            texture.Apply(false, makeNoLongerReadable);
         }
 
         /// <summary>
         /// sets a 1 pixel border of the texture on all mipmap levels to clear white
         /// </summary>
         /// <param name="texture"></param>
-        public static void ClearMipMapBorders(this Texture2D texture)
+        /// <param name="makeNoLongerReadable"></param>
+        public static void ClearMipMapBorders(this Texture2D texture, bool makeNoLongerReadable = false)
         {
             var clear = new Color(1, 1, 1, 0);
-            ClearMipMapBorders(texture, clear);
+            ClearMipMapBorders(texture, clear, makeNoLongerReadable);
         }
     }
 }
